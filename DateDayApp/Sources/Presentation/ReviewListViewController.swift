ffe2880c5dc3c6090c8f91b1b9337b51197837e8
yeggrrr <yjkim981103@gmail.com>
2024-08-15 19:49:49 +0900

    [Add] 폴더 추가

--- DateDayApp/Sources/Presentation/ReviewListViewController.swift
+++ DateDayApp/Sources/Presentation/ReviewListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ReviewListViewController.swift
 //  DateDayApp
 //
 //  Created by YJ on 8/14/24.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ReviewListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        view.backgroundColor = .white
     }
 }
 
