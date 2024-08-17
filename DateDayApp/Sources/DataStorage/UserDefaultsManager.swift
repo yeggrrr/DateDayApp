d44b9493d7df2388d5e83690cea7804c2fbe6deb
yeggrrr <yjkim981103@gmail.com>
2024-08-17 21:10:15 +0900

    [Feat] UserDefaultsManager 추가

---
+++ DateDayApp/Sources/DataStorage/UserDefaultsManager.swift
@@ -0,0 +1,39 @@
+//
+//  UserDefaultsManager.swift
+//  DateDayApp
+//
+//  Created by YJ on 8/17/24.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    private enum UserDefaultKey: String {
+        case access
+        case refresh
+    }
+    
+    static let shared = UserDefaultsManager()
+    
+    private init() { }
+    
+    var token: String {
+        get {
+            UserDefaults.standard.string(forKey: UserDefaultKey.access.rawValue) ?? ""
+        }
+        
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: UserDefaultKey.access.rawValue)
+        }
+    }
+    
+    var refresh: String {
+        get {
+            UserDefaults.standard.string(forKey: UserDefaultKey.refresh.rawValue) ?? ""
+        }
+        
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: UserDefaultKey.refresh.rawValue)
+        }
+    }
+}
